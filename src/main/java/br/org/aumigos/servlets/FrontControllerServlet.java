6e71298c3c8b3f2f801b41c65c5746ec8ae46592
Eduardo Soares <eduardo_soares001@outlook.com>
2024-11-28 14:54:58 -0300

    finish class HelperFactory

--- src/main/java/br/org/aumigos/servlets/FrontControllerServlet.java
+++ src/main/java/br/org/aumigos/servlets/FrontControllerServlet.java
@@ -1,5 +1,9 @@
 package br.org.aumigos.servlets;
 
+import br.org.aumigos.servlets.helpers.Helper;
+import br.org.aumigos.servlets.helpers.HelperFactory;
+import com.google.gson.JsonObject;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,6 +22,24 @@ public class FrontControllerServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Helper helper = new HelperFactory().getHelper(request);
 
+        try{
+            Object resp = helper.execute(request, response);
+            if(resp instanceof JsonObject){
+                response.setContentType("application/json");
+                response.getWriter().write(response.toString());
+            }else{
+                RequestDispatcher dispatcher = request.getRequestDispatcher(response.toString());
+                dispatcher.forward(request, response);
+            }
+        }catch(Exception error){
+            throw new ServletException(error);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
     }
 }
