e303218cac98e319680772a0b6ce24237e29e039
Eduardo Soares <eduardo_soares001@outlook.com>
2024-11-28 14:39:51 -0300

    Classe Helper

--- src/main/java/br/org/aumigos/servlets/FrontControllerServlet.java
+++ src/main/java/br/org/aumigos/servlets/FrontControllerServlet.java
@@ -1,4 +1,23 @@
 package br.org.aumigos.servlets;
 
-public class FrontControllerServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/ControllerServlet")
+public class FrontControllerServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public FrontControllerServlet(){
+        super();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
